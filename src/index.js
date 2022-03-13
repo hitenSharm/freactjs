7c5dd255bda02a9117dac56b6e8a163cbec8e7ed
HitenSharma <hiten.sharm1708@gmail.com>
2022-03-13 11:45:35 +0530

    created object creater

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
-const element ={
-  type:"h1",
-  props:{
-    title:"fubar",
-    children:"Text child"
+function createElement(type,props,...children)
+{
+  return {
+    type,
+    props:{
+      ...props,
+      children:children.map(child =>{
+        typeof child === "object" ? child : createTextElement(child)
+      })
+    }
   }
-};
-//jsx is turned into js using Babel
-//const element =<h1 title="fubar">Text child</h1> this is JSX; not valid JS
-//to turn into valid JS i need createElement
-//we can use React.createElement but we need to replace it with our own to make framework
+}
+
+//as any child could be something primitive like text/numbers we need a secial function just for those nodes
+
+function createTextElement(text)
+{
+  return{
+    type:"TEXT_ELEMENT",
+    props:{
+      nodeValue:text,
+      children:[],
+    }
+  }
+}
+
+const element = (
+  <div id="bar">
+    <h1>Child below is empty</h1>
+    <h2></h2>
+  </div>
+)
+
+// we need to create our own React.createElement type function which takes jsx and turns it to js
+//a create element function basically creates an object with types and props
+
 
 const container = document.getElementById("root")
-
-//we also need to replace ReactDom.render
-//so we need to create a node using the element
-
-const node = document.createElement(element.type);
-//create a node of type element.type and give it a title
-node["title"]=element.props.title;
-
-//next we need to work on children. i.e. create children nodes
-//here we have a string in children so we need to create a text node
-
-const text=document.createTextNode(element.props.children)
-
-//append to original node
-node.appendChild(text);
-//append to container
-container.appendChild(node);
-
-//---------------------------first iteration of a basic JavaScript only rendering system for text---------
-
-
-
